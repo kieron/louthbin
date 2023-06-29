e0a3551d75515d8f22079a734549baedb1e39d3b
Kieron Boswell <kieron.boswell@asurafin.com>
2023-06-29 23:14:56 +0100

    tidy

--- pages/index.vue
+++ pages/index.vue
@@ -1,185 +1,189 @@
 <script lang="ts" setup>
 // composable
-
+import { format } from 'date-fns'
 // meta
 definePageMeta({
   layout: 'page',
 })
-
-const binData = [
+export interface IBinData {
+  date: string
+  name: string
+}
+const binData: IBinData[] = [
   {
-    date: '06-06-2023',
+    date: '2023-06-06',
     name: 'domestic waste'
   },
   {
-    date: '13-06-2023',
+    date: '2023-06-13',
     name: 'recycling'
   },
   {
-    date: '20-06-2023',
+    date: '2023-06-20',
     name: 'domestic waste'
   },
   {
-    date: '27-06-2023',
+    date: '2023-06-27',
     name: 'paper and card'
   },
   {
-    date: '04-07-2023',
+    date: '2023-07-04',
     name: 'domestic waste'
   },
   {
-    date: '11-07-2023',
+    date: '2023-07-11',
     name: 'recycling'
   },
   {
-    date: '18-07-2023',
+    date: '2023-07-18',
     name: 'domestic waste'
   },
   {
-    date: '25-07-2023',
+    date: '2023-07-25',
     name: 'paper and card'
   },
   {
-    date: '01-08-2023',
+    date: '2023-08-01',
     name: 'domestic waste'
   },
   {
-    date: '08-08-2023',
+    date: '2023-08-08',
     name: 'recycling'
   },
   {
-    date: '15-08-2023',
+    date: '2023-08-15',
     name: 'domestic waste'
   },
   {
-    date: '22-08-2023',
+    date: '2023-08-22',
     name: 'paper and card'
   },
   {
-    date: '29-08-2023',
+    date: '2023-08-29',
     name: 'domestic waste'
   },
   {
-    date: '05-09-2023',
+    date: '2023-09-05',
     name: 'recycling'
   },
   {
-    date: '12-09-2023',
+    date: '2023-09-12',
     name: 'domestic waste'
   },
   {
-    date: '19-09-2023',
+    date: '2023-09-19',
     name: 'paper and card'
   },
   {
-    date: '26-09-2023',
+    date: '2023-09-26',
     name: 'domestic waste'
   },
   {
-    date: '03-10-2023',
+    date: '2023-10-03',
     name: 'recycling'
   },
   {
-    date: '10-10-2023',
+    date: '2023-10-10',
     name: 'domestic waste'
   },
   {
-    date: '17-10-2023',
+    date: '2023-10-17',
     name: 'paper and card'
   },
   {
-    date: '24-10-2023',
+    date: '2023-10-24',
     name: 'domestic waste'
   },
   {
-    date: '31-10-2023',
+    date: '2023-10-31',
     name: 'recycling'
   },
   {
-    date: '07-11-2023',
+    date: '2023-11-07',
     name: 'domestic waste'
   },
   {
-    date: '14-11-2023',
+    date: '2023-11-14',
     name: 'paper and card'
   },
   {
-    date: '21-11-2023',
+    date: '2023-11-21',
     name: 'domestic waste'
   },
   {
-    date: '28-11-2023',
+    date: '2023-11-28',
     name: 'recycling'
   },
   {
-    date: '05-12-2023',
+    date: '2023-12-05',
     name: 'domestic waste'
   },
   {
-    date: '12-12-2023',
+    date: '2023-12-12',
     name: 'paper and card'
   },
   {
-    date: '19-12-2023',
+    date: '2023-12-19',
     name: 'domestic waste'
   },
   {
-    date: '26-12-2023',
+    date: '2023-12-26',
     name: 'NO COLLECTION'
   },
   {
-    date: '02-01-2024',
+    date: '2024-01-02',
     name: 'domestic waste && recycling'
   },
   {
-    date: '09-01-2024',
+    date: '2024-01-09',
     name: 'paper and card'
   },
   {
-    date: '16-01-2024',
+    date: '2024-01-16',
     name: 'domestic waste'
   },
   {
-    date: '23-01-2024',
+    date: '2024-01-23',
     name: 'recycling'
   },
   {
-    date: '30-01-2024',
+    date: '2024-01-30',
     name: 'domestic waste'
   },
   {
-    date: '06-02-2024',
+    date: '2024-02-06',
     name: 'paper and card'
   },
   {
-    date: '13-02-2024',
+    date: '2024-02-13',
     name: 'domestic waste'
   },
   {
-    date: '20-02-2024',
+    date: '2024-02-20',
     name: 'recycling'
   },
   {
-    date: '27-02-2024',
+    date: '2024-02-27',
     name: 'domestic waste'
   },
   {
-    date: '05-03-2024',
+    date: '2024-03-05',
     name: 'paper and card'
   },
   {
-    date: '12-03-2024',
+    date: '2024-03-12',
     name: 'domestic waste'
   },
   {
-    date: '19-03-2024',
+    date: '2024-03-19',
     name: 'recycling'
   },
   {
-    date: '26-03-2024',
+    date: '2024-03-26',
     name: 'domestic waste'
   },
-]
+];
+
 
 </script>
 
@@ -199,15 +203,10 @@ const binData = [
     </div>
     <PageBody class="flex-1 flex">
       <PageSection class="flex-1 flex items-center">
-        <div class="flex-1 md:w-5/8 flex flex-col z-10">
-          <h1 class="text-center md:text-left mt-4">
-            <span
-              class="animated-text-bg drop-shadow-xl text-4xl xl:text-5xl 2xl:text-6xl block font-black uppercase"
-            >
-              <span class="">wat bin</span>
-              {{binData}}
-            </span>
-          </h1>
+        <div class="flex-1 flex flex-col z-10">
+          <div :key="index" v-for="day, index in binData">
+              {{format(new Date(day.date), 'dd MMM yyyy')}} - {{day.name}}
+          </div>
         </div>
       </PageSection>
     </PageBody>
