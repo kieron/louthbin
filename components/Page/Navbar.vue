8c63bfecf83e046b7bb9e895e62d785fc5d9beb7
Kieron Boswell <kieron.boswell@asurafin.com>
2023-06-29 22:17:18 +0100

    more unneeded

--- components/Page/Navbar.vue
+++ components/Page/Navbar.vue
@@ -62,48 +62,5 @@ const menus = computed((): IMenuItem[] => [
 
       </div>
     </template>
-    <template #options="{ toggleOptions }">
-      <ActionSheet @on-close="toggleOptions(false)">
-        <ActionSheetBody>
-          <ActionSheetHeader text="Menu" />
-          <nav class="leading-6 font-semibold text-gray-600 dark:text-gray-300">
-            <ul class="flex flex-col">
-              <li
-                v-for="(item, i) in menus"
-                :key="i"
-                class="flex w-full"
-                :class="{
-                  'pb-2 mb-2 border-b border-gray-900/10 dark:border-gray-50/[0.2]':
-                    item.type === 'link',
-                }"
-              >
-                <Anchor
-                  v-if="item.type === 'link'"
-                  :to="item.route ? item.route : undefined"
-                  :href="item.href ? item.href : undefined"
-                  class="flex-1 hover:no-underline capitalize"
-                  >{{ item.text }}</Anchor
-                >
-                <Button
-                  v-else-if="item.type === 'button'"
-                  :text="item.text"
-                  size="xs"
-                  class="flex-1 font-extrabold capitalize"
-                  :to="item.route ? item.route : undefined"
-                  :href="item.href ? item.href : undefined"
-                />
-              </li>
-            </ul>
-          </nav>
-          <div class="mt-6 text-sm font-bold capitalize">Theme</div>
-
-        </ActionSheetBody>
-        <Button
-          text="Close"
-          type="secondary"
-          @click.prevent="toggleOptions(false)"
-        />
-      </ActionSheet>
-    </template>
   </BuilderNavbar>
 </template>
