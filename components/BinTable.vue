4afab898126bf7599ea7dbc8a1c7719f9fa5e77f
Kieron Boswell <kieron.boswell@snappyshopper.co.uk>
2024-04-16 19:34:39 +0100

    Update bin collection dates

--- components/BinTable.vue
+++ components/BinTable.vue
@@ -2,206 +2,58 @@
 import { format, isToday, isTomorrow, isTuesday } from "date-fns";
 
 const binData = [
-  {
-    date: "2024-04-16",
-    name: "recycling",
-  },
-  {
-    date: "2024-04-23",
-    name: "domestic waste",
-  },
-  {
-    date: "2024-04-30",
-    name: "paper and card",
-  },
-  {
-    date: "2024-05-07",
-    name: "domestic waste",
-  },
-  {
-    date: "2024-05-14",
-    name: "recycling",
-  },
-  {
-    date: "2024-05-21",
-    name: "domestic waste",
-  },
-  {
-    date: "2024-05-28",
-    name: "paper and card",
-  },
-  {
-    date: "2024-06-04",
-    name: "domestic waste",
-  },
-  {
-    date: "2024-06-11",
-    name: "recycling",
-  },
-  {
-    date: "2024-06-18",
-    name: "domestic waste",
-  },
-  {
-    date: "2024-06-25",
-    name: "paper and card",
-  },
-  {
-    date: "2024-07-02",
-    name: "domestic waste",
-  },
-  {
-    date: "2024-07-09",
-    name: "recycling",
-  },
-  {
-    date: "2024-07-16",
-    name: "domestic waste",
-  },
-  {
-    date: "2024-07-23",
-    name: "paper and card",
-  },
-  {
-    date: "2024-07-30",
-    name: "domestic waste",
-  },
-  {
-    date: "2024-08-06",
-    name: "recycling",
-  },
-  {
-    date: "2024-08-13",
-    name: "domestic waste",
-  },
-  {
-    date: "2024-08-20",
-    name: "paper and card",
-  },
-  {
-    date: "2024-08-27",
-    name: "domestic waste",
-  },
-  {
-    date: "2024-09-03",
-    name: "recycling",
-  },
-  {
-    date: "2024-09-10",
-    name: "domestic waste",
-  },
-  {
-    date: "2024-09-17",
-    name: "paper and card",
-  },
-  {
-    date: "2024-09-24",
-    name: "domestic waste",
-  },
-  {
-    date: "2024-10-01",
-    name: "recycling",
-  },
-  {
-    date: "2024-10-08",
-    name: "domestic waste",
-  },
-  {
-    date: "2024-10-15",
-    name: "paper and card",
-  },
-  {
-    date: "2024-10-22",
-    name: "domestic waste",
-  },
-  {
-    date: "2024-10-29",
-    name: "recycling",
-  },
-  {
-    date: "2024-11-05",
-    name: "domestic waste",
-  },
-  {
-    date: "2024-11-12",
-    name: "paper and card",
-  },
-  {
-    date: "2024-11-19",
-    name: "domestic waste",
-  },
-  {
-    date: "2024-11-26",
-    name: "recycling",
-  },
-  {
-    date: "2024-12-03",
-    name: "domestic waste",
-  },
-  {
-    date: "2024-12-10",
-    name: "paper and card",
-  },
-  {
-    date: "2024-12-17",
-    name: "domestic waste",
-  },
-  {
-    date: "2024-12-24",
-    name: "NO COLLECTION",
-  },
-  {
-    date: "2024-12-31",
-    name: "domestic waste",
-  },
-  {
-    date: "2025-01-07",
-    name: "recycling",
-  },
-  {
-    date: "2025-01-14",
-    name: "domestic waste",
-  },
-  {
-    date: "2025-01-21",
-    name: "paper and card",
-  },
-  {
-    date: "2025-01-28",
-    name: "domestic waste",
-  },
-  {
-    date: "2025-02-04",
-    name: "recycling",
-  },
-  {
-    date: "2025-02-11",
-    name: "domestic waste",
-  },
-  {
-    date: "2025-02-18",
-    name: "paper and card",
-  },
-  {
-    date: "2025-02-25",
-    name: "domestic waste",
-  },
-  {
-    date: "2025-03-04",
-    name: "recycling",
-  },
-  {
-    date: "2025-03-11",
-    name: "domestic waste",
-  },
-  {
-    date: "2025-03-18",
-    name: "paper and card",
-  },
-  {
-    date: "2025-03-25",
-    name: "domestic waste",
-  },
+  { date: "2024-04-02", name: "paper and card" },
+  { date: "2024-04-09", name: "domestic waste" },
+  { date: "2024-04-16", name: "recycling" },
+  { date: "2024-04-23", name: "domestic waste" },
+  { date: "2024-04-30", name: "paper and card" },
+  { date: "2024-05-07", name: "domestic waste" },
+  { date: "2024-05-14", name: "recycling" },
+  { date: "2024-05-21", name: "domestic waste" },
+  { date: "2024-05-28", name: "paper and card" },
+  { date: "2024-06-04", name: "domestic waste" },
+  { date: "2024-06-11", name: "recycling" },
+  { date: "2024-06-18", name: "domestic waste" },
+  { date: "2024-06-25", name: "paper and card" },
+  { date: "2024-07-02", name: "domestic waste" },
+  { date: "2024-07-09", name: "recycling" },
+  { date: "2024-07-16", name: "domestic waste" },
+  { date: "2024-07-23", name: "paper and card" },
+  { date: "2024-07-30", name: "domestic waste" },
+  { date: "2024-08-06", name: "recycling" },
+  { date: "2024-08-13", name: "domestic waste" },
+  { date: "2024-08-20", name: "paper and card" },
+  { date: "2024-08-27", name: "domestic waste" },
+  { date: "2024-09-03", name: "recycling" },
+  { date: "2024-09-10", name: "domestic waste" },
+  { date: "2024-09-17", name: "paper and card" },
+  { date: "2024-09-24", name: "domestic waste" },
+  { date: "2024-10-01", name: "recycling" },
+  { date: "2024-10-08", name: "domestic waste" },
+  { date: "2024-10-15", name: "paper and card" },
+  { date: "2024-10-22", name: "domestic waste" },
+  { date: "2024-10-29", name: "recycling" },
+  { date: "2024-11-05", name: "domestic waste" },
+  { date: "2024-11-12", name: "paper and card" },
+  { date: "2024-11-19", name: "domestic waste" },
+  { date: "2024-11-26", name: "recycling" },
+  { date: "2024-12-03", name: "domestic waste" },
+  { date: "2024-12-10", name: "paper and card" },
+  { date: "2024-12-17", name: "domestic waste" },
+  { date: "2024-12-24", name: "recycling" },
+  { date: "2024-12-31", name: "domestic waste" },
+  { date: "2025-01-07", name: "paper and card" },
+  { date: "2025-01-14", name: "domestic waste" },
+  { date: "2025-01-21", name: "recycling" },
+  { date: "2025-01-28", name: "domestic waste" },
+  { date: "2025-02-04", name: "paper and card" },
+  { date: "2025-02-11", name: "domestic waste" },
+  { date: "2025-02-18", name: "recycling" },
+  { date: "2025-02-25", name: "domestic waste" },
+  { date: "2025-03-04", name: "paper and card" },
+  { date: "2025-03-11", name: "domestic waste" },
+  { date: "2025-03-18", name: "recycling" },
+  { date: "2025-03-25", name: "domestic waste" },
 ];
 
 const currentBinData = binData.filter((bin) => {
