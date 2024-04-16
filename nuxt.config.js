1de61a93ba743d727aa6e9fcaec7e0d33c4e13b1
Kieron Boswell <kieron.boswell@snappyshopper.co.uk>
2024-04-16 19:55:13 +0100

    Update bin collection dates in nuxt.config.js and package.json

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,19 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: ["@nuxtjs/tailwindcss"],
   tailwindcss: {
     // Options
   },
   head: {
-    title: 'louthbin',
+    title: "Louth Bin Schedule",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Louth bin day' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: "Louth Bin Schedule - Sorry if its wrong.",
+      },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.png" }],
   },
-})
+});
