87d044b6acca12bfabd913138e7809e7d96b262c
Kieron Boswell <kieron.boswell@snappyshopper.co.uk>
2024-04-16 19:58:22 +0100

    Update meta tags in index.vue component

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,20 @@
 <script setup>
+useHead({
+  title: "Louth Bin Schedule",
+  meta: [
+    {
+      name: "description",
+      content: "Louth Bin Schedule - Sorry if its wrong.",
+    },
+  ],
+});
 </script>
 
 <template>
   <div>
-    <div class="w-full h-6 bg-green-300 flex justify-center items-center"><span class="text-gray-900 text-xs">✌️ sry if its wrong ~ kieron</span></div>
+    <div class="w-full h-6 bg-green-300 flex justify-center items-center">
+      <span class="text-gray-900 text-xs">✌️ sry if its wrong ~ kieron</span>
+    </div>
     <BinTable />
   </div>
 </template>
